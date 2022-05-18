be0ad510a7ff9eaf326b081835232f44e5219393
Tony Bowden <tony@tmtm.com>
2022-05-18 05:23:32 +0300

    compare Prime Minister

---
+++ sources/prime-minister/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name img start end].freeze
+    end
+
+    def name_node
+      name_cell.css('a').first
+    end
+
+    def ignore_before
+      1979
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
