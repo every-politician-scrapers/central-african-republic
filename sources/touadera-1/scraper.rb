6e6290a2b91828e8b8663155d051a81619d24098
Tony Bowden <tony@tmtm.com>
2022-05-19 16:59:41 +0300

    compare Gouvernement Touadéra I

---
+++ sources/touadera-1/scraper.rb
@@ -0,0 +1,37 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class MemberList
+  class Members
+    decorator RemoveReferences
+    # decorator WikidataIdsDecorator::Links
+
+    def member_container
+      noko.xpath('//h3[contains(., "inistre") or contains(.,"Secrétaire")]//following-sibling::ul[1]//li')
+    end
+
+    def member_items
+      super.reject { |mem| mem.name.to_s.empty? }
+    end
+  end
+
+  class Member
+    field :name do
+      position_and_name.last.gsub(/\(.*?\)/, '').tidy
+    end
+
+    field :position do
+      position_and_name.first.gsub(/^\d+\./, '').tidy
+    end
+
+    def position_and_name
+      noko.text.split(':', 2)
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url).csv
